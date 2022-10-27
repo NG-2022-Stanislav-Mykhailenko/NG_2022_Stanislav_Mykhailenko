ca319994ca0070a892a024119593e40768c2b2d7
Stanislav Mykhailenko <mykhailenkostanislavzno@gmail.com>
2022-10-27 18:38:13 +0300

    Add Lesson 2 Task %*f#ncA0#>?

---
+++ Lesson_2/We know/dumb_decryptor.py
@@ -0,0 +1,10 @@
+# Lesson 2 Task %*f#ncA0#>?: ROT13 encryption
+# Author: Stanislav Mykhailenko
+# License: Unlicense
+
+# Return codes:
+# 0 - OK
+
+import codecs
+
+print(codecs.encode(input("Enter message: "), "rot13"))
